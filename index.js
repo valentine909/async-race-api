e09858d23271c9ef28ec0671cfe5c3b2a7e14595
valentine909 <valentine.nazarov@gmail.com>
2022-01-27 13:02:23 +0300

    feat: change port to heroku compatible

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const server = jsonServer.create();
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 const state = { velocity: {}, blocked: {} };
 
